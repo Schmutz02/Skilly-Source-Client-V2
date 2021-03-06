16f430312a1abe2998f0e6dbf9e43790351958d6
Schmutz02 <31942157+Schmutz02@users.noreply.github.com>
2021-03-06 16:27:12 -0500

    Added settings class
    Store global application data

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,7 @@
+public static class Settings
+{
+    private const int PORT = 7777;
+    private const string IP_ADDRESS = "127.0.0.1";
+
+    public static readonly string ServerURL = $"http://{IP_ADDRESS}:{PORT}";
+}
