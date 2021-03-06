02fd6ce821ec5b2db521cebf65516954b8698722
Schmutz02 <31942157+Schmutz02@users.noreply.github.com>
2021-03-06 16:42:42 -0500

    Added WebRequest
    Base class for all web requests

--- Assets/Scripts/WebRequest.cs
+++ Assets/Scripts/WebRequest.cs
@@ -1,6 +1,27 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
 
-public class WebRequest : MonoBehaviour
+public abstract class WebRequest
 {
-    
+    public Action<string, bool> Callback { get; }
+    public KeyValuePair<string, string>[] Content { get; }
+
+    private string _request;
+
+    protected WebRequest(string request, Action<string, bool> callback, KeyValuePair<string, string>[] content = null)
+    {
+        _request = request;
+        Callback = callback;
+        Content = content ?? new KeyValuePair<string, string>[0];
+    }
+
+    public string GetUrl()
+    {
+        if (_request[0] != '/')
+        {
+            _request = "/" + _request;
+        }
+
+        return Settings.ServerURL + _request;
+    }
 }
