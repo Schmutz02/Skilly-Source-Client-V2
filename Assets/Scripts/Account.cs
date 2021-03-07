e1f5ce45454f5713d19158255ec4b04d434166f0
Schmutz02 <31942157+Schmutz02@users.noreply.github.com>
2021-03-06 20:49:07 -0500

    Added Logging In

--- Assets/Scripts/Account.cs
+++ Assets/Scripts/Account.cs
@@ -1,15 +1,24 @@
+using UnityEngine;
+
 public static class Account
 {
+    public const string USERNAME_KEY = "username";
     public static string Username { get; private set; }
 
-    public static void Login(string username)
+    public static void Login(string username, bool rememberUsername)
     {
         Reset();
         Username = username;
+
+        if (rememberUsername)
+        {
+            PlayerPrefs.SetString(USERNAME_KEY, username);
+        }
     }
 
     private static void Reset()
     {
         Username = null;
+        PlayerPrefs.DeleteKey(USERNAME_KEY);
     }
 }
--- Assets/Scripts/Tasks/LogInTask.cs
+++ Assets/Scripts/Tasks/LogInTask.cs
@@ -7,13 +7,15 @@ namespace Tasks
     {
         private readonly string _username;
         private readonly string _password;
+        private readonly bool _rememberUsername;
     
-        public LogInTask(string username, string password)
+        public LogInTask(string username, string password, bool rememberUsername)
         {
             _username = username;
             _password = password;
+            _rememberUsername = rememberUsername;
         }
-    
+
         public override void StartAsync()
         {
             var logInRequest = new LogInRequest(OnRequestComplete, _username, _password);
@@ -24,7 +26,7 @@ namespace Tasks
         {
             if (success)
             {
-                Account.Login(_username);
+                Account.Login(_username, _rememberUsername);
             }
             IsComplete = true;
         }
